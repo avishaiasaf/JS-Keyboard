e25d20ccce05ccc4733cfc57e772b8334b727443
avishaiasaf <64068379+avishaiasaf@users.noreply.github.com>
2021-03-21 20:55:42 +0200

    Fourth Method - Hypothesis

--- Math/Comparing_Pythagorean_triple_methods.js
+++ Math/Comparing_Pythagorean_triple_methods.js
@@ -15,6 +15,22 @@ function isTriple(a, b, c){
     return a**2 + b**2 == c**2;
 }
 
+function secondMethod(num){
+    var count_logger=0;
+    for(let n=1;n<num/2;n++){
+      for(let m=n+1;m<num/2;m++){
+           count_logger++;
+           let a = m**2 - n**2;
+           let b = 2*m*n;
+           let c = m**2 + n**2;
+           if(a+b+c==num){
+               return [[a, b, c], count_logger];
+           }
+       }
+   }
+   return 'No Pythagorian triple.';
+}
+
 function thirdMethod(num){
     var i=0;
     for(let m=1;m<Math.sqrt(num);m++){
@@ -25,41 +41,39 @@ function thirdMethod(num){
             let b = 2*m*n;
             let c = m**2 + n**2;
             if(a + b + c == num){
-                return [[a, b, c], i];
+                return [[a, b, c], i, [m, n, Math.sqrt(num)]];
             }            
         }
     }
     return 'No Pythagorian triple.';
 }
   
-function secondMethod(num){
-     var count_logger=0;
-     for(let n=1;n<num/2;n++){
-       for(let m=n+1;m<num/2;m++){
-            count_logger++;
+
+function fourthMethod(num){
+    var i=0;
+    for(let m=Math.floor(Math.sqrt(num)*0.49);m<Math.ceil(Math.sqrt(num)*0.71);m++){
+        let n = ((num/2)/m) - m;
+        i++;
+        if(Number.isInteger(n) && m>n){
             let a = m**2 - n**2;
             let b = 2*m*n;
             let c = m**2 + n**2;
-            if(a+b+c==num){
-                return [[a, b, c], count_logger];
-            }
+            if(a + b + c == num){
+                return [[a, b, c], i, [m, n, Math.sqrt(num)]];
+            }            
         }
     }
     return 'No Pythagorian triple.';
 }
    
 function compareMethods(n){
-    var a = firstMethod(n);
-    var b = secondMethod(n);
-    var c = thirdMethod(n);
-    console.log('function a solution: [' + a[0] + ']');
-    console.log('number of iterations: ' + a[1]);
-    console.log('function b solution: [' + b[0] + ']');
-    console.log('number of iterations: ' + b[1]);
-    console.log('function c solution: [' + c[0] + ']');
-    console.log('number of iterations: ' + c[1]);
+    var results = [firstMethod(n), secondMethod(n), thirdMethod(n), fourthMethod(n)];
+    for(let i=0;i<results.length;i++){
+        console.log('function ' + i + ' solution: ['+ results[i][0] + ']');
+        console.log('number of iterations: ' + results[i][1]);
+    }
 }
-   
+  
 //compareMethods(1000);
 
 //function a solution: [200,375,425]
@@ -68,3 +82,5 @@ function compareMethods(n){
 //number of iterations: 2,001
 //function c solution: [375,200,425]
 //number of iterations: 20
+//function d solution: [375,200,425]
+//number of iterations: 6
